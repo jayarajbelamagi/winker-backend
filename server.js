657f1453c554d96bf428caf030f9eae7d0ca6c1d
Jayaraj U B <ubelamagi@gmail.com>
2025-09-15 23:26:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -36,14 +36,14 @@ app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 app.use("/api/stories", storyRoutes);
 
-if(process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "../frontend/dist")));
+//if(process.env.NODE_ENV === "production") {
+	//app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
-app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
-	connectMongoDB();
-});
+	//app.get("*", (req, res) => {
+	//	res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	//});
+//}
+//app.listen(PORT, () => {
+	//console.log(`Server is running on port ${PORT}`);
+	//connectMongoDB();
+//});
